d8bc1c49cb43cf5410141868dd4e210c1e0e9119
Dinar <dffaiskhanov@edu.hse.ru>
2020-08-11 01:59:00 +0300

    added automapper

---
+++ TestingService.Api/Mapping/AutoMapping.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using TestInfoMongo = TestingService.Domain.Repositories.Entities.TestInfo.Test;
+using TestInfoDomain = TestingService.Domain.Entities.TestInfo.TestInfo;
+
+namespace TestingService.Api.Mapping
+{
+    public class AutoMapping : Profile
+    {
+        public AutoMapping()
+        {
+            CreateMap<TestInfoMongo, TestInfoDomain>();
+            CreateMap<TestInfoDomain, TestInfoMongo>();
+        }
+    }
+}
--- TestingService.Api/Startup.cs
+++ TestingService.Api/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AutoMapper;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -10,6 +11,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using TestingService.Api.Mapping;
 
 namespace TestingService
 {
@@ -26,6 +28,7 @@ namespace TestingService
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddAutoMapper(c => c.AddProfile<AutoMapping>(), typeof(Startup));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
